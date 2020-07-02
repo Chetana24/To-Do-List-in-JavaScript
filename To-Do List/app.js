52d827453bac93916a22dd96364fa3e885d254f7
Chetana24 <55477043+Chetana24@users.noreply.github.com>
2020-07-02 19:56:10 +0530

    Add files via upload

---
+++ To-Do List/app.js
@@ -0,0 +1,89 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
+//event listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+
+//functions
+
+function addTodo(event){
+	//prevent form from submitting
+	event.preventDefault();
+	
+		//TODO DIV
+	const todoDiv = document.createElement("div");
+	todoDiv.classList.add("todo");
+
+		//CREATE LI
+	const newTodo = document.createElement("li");
+	newTodo.innerText = todoInput.value;
+	newTodo.classList.add("todo-item");
+	todoDiv.appendChild(newTodo);
+
+		//CHECK MARK BUTTON
+	const completedButton = document.createElement("button");
+	completedButton.innerHTML = '<i class="fa fa-check"></i>';
+	completedButton.classList.add("complete-btn");
+	todoDiv.appendChild(completedButton);
+
+		//DELETE MARK BUTTON
+	const trashButton = document.createElement("button");
+	trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+	trashButton.classList.add("trash-btn");
+	todoDiv.appendChild(trashButton);
+
+		//APPEND TO LIST
+	todoList.appendChild(todoDiv);
+
+		//CLEAR TODOINPUT VALUE
+	todoInput.value = "";
+
+}
+
+
+function deleteCheck(e){
+	const item = e.target;
+
+		//DELETE TODO 
+	if(item.classList[0] === "trash-btn") {
+		const todo = item.parentElement;
+			todo.remove();
+	}
+
+		//CHECK MARK
+	if(item.classList[0] === "complete-btn") {
+		const todo = item.parentElement;
+		todo.classList.toggle("completed");
+	}
+}
+
+function filterTodo(e){
+	const todos = todoList.childNodes;
+	todos.forEach(function(todo){
+		switch(e.target.value){
+			case "all":
+					todo.style.display = "flex";
+				break;
+			case "completed":
+				if(todo.classList.contains('completed')){
+					todo.style.display = "flex";
+				}
+				else{
+					todo.style.display = "none";
+				}
+				break;
+			case "uncompleted":
+				if(!todo.classList.contains("completed")){
+					todo.style.display = "flex";
+				}else{
+					todo.style.display = "none";
+				}
+				break;
+		}
+	});
+}
